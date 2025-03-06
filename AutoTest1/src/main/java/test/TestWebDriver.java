d6d2deeff1d7b3f7ec2dd4e20f54ab3ca422d6e9
dongducan10 <126158559+dongducan10@users.noreply.github.com>
2025-03-06 15:15:08 +0700

    Update TestWebDriver.java

--- AutoTest1/src/main/java/test/TestWebDriver.java
+++ AutoTest1/src/main/java/test/TestWebDriver.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeOptions;áhbashbdadasdasdasdasdasdasdadsdss
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
